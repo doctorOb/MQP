d6f0d8ab0ba63b2d256b9c67a5f53964c3202d00
Dan Robertson <danrobertson@wifi-130-215-11-187.dyn.wpi.edu>
2013-09-20 13:48:08 -0400

    asdf

---
+++ smallProx/mcprox.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+ 
+"""
+Microproxy
+This code is based on code based on microproxy.py written by ubershmekel in 2006.
+ 
+Microproxy is the simplest possible http proxy. It simply relays all bytes from the client to the server at a socket send and recv level. The way it recognises the remote server to connect to is by a simple regex, which extracts the URL of the origin server from the byte stream. (This probably doesn't work in all cases).
+ 
+"""
+ 
+import re, time, sys
+import socket
+import threading
+ 
+ 
+PORT = 8080
+HOST = socket.gethostbyname(socket.gethostname())
+regex = re.compile(r'http://(.*?)/', re.IGNORECASE)
+ 
+class ConnectionThread(threading.Thread):
+	def __init__(self, (conn,addr)):
+		self.conn = conn
+		self.addr = addr
+		threading.Thread.__init__(self)
+   
+	def run(self):
+ 
+		data = self.conn.recv(1024*1024)
+		host = regex.search(data).groups()[0]
+	   
+		request = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		#request.settimeout(6)
+		request.connect((host,80))
+		request.send(data)
+ 
+		reply = ''
+ 
+		while 1:
+			print"waiting for host"
+			temp = request.recv(1024)
+ 
+			if ('' == temp):
+				break
+			   
+			self.conn.send(temp)
+		self.conn.close()
+ 
+class ProxyThread(threading.Thread):
+	def __init__(self, host, port):
+		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.sock.bind((host, port))
+		print"before threading start"
+		threading.Thread.__init__(self)
+		print"after threading start"
+   
+	def run(self):
+		self.sock.listen(10)
+		while 1:
+			print"still alive"
+			temp = ConnectionThread(self.sock.accept())
+			print"<<<<<<<connection from host read>>>>>>>"
+			temp.daemon = True
+			temp.start()
+ 
+if __name__ == "__main__":
+	proxy = ProxyThread(HOST,PORT)
+	proxy.daemon = True
+	proxy.start()
+	print "Started a proxy on port", PORT, "with hostname", HOST
