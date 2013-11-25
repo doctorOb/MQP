ec8a6bde6382221c9ddb8b4d64d435703f0d5934
Dan Robertson <danrobertson@drob.wifi.wpi.edu>
2013-11-25 14:41:07 -0500

    didnt actually fix

--- pullLoop.sh
+++ pullLoop.sh
@@ -2,8 +2,8 @@
 
 while true
 do
-	git fetch origin master &> /dev/null;
-	git reset --hard FETCH_HEAD &> /dev/null;
-	git clean -df &> /dev/null;
+	git fetch origin master;
+	git reset --hard FETCH_HEAD;
+	git clean -df;
 	sleep 20;
 done
