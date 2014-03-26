b50eb76fc51214b0eae10cfa89be0b7d9674c851
Dan Robertson <drob.drobertson@gmail.com>
2014-03-26 16:45:04 -0400

    finally fixed logger

--- code/prototype/PybaProx/Logger.py
+++ code/prototype/PybaProx/Logger.py
@@ -58,23 +58,34 @@ class Logger(object):
 
 		if self.options['display_caller']:
 			try:
-				caller_method = "{}".format(stack[1][0].f_code.co_name)
-				caller_class = stack[1][0].f_locals["self"].__class__
+				caller_method = "{}".format(stack[2][0].f_code.co_name)
+				caller_class = stack[2][0].f_locals["self"].__class__
 			except:
 				pass
 		if self.options['display_time']:
 			date = time.strftime("%X")
 
-		self._log("<{}>[{}.{}]@|{}|# {}".format(log_type,caller_class,caller_method,date,msg))
+		self._log("<({})[{}.{}]|{}|> {}".format(log_type,caller_class,caller_method,date,msg))
 
 	def info(self,msg):
-		self._format("info",msg)
+		self._format("INFO",msg)
 
 	def logic(self,msg):
-		self._format("logic",msg)
+		self._format("LOGIC",msg)
 
 	def warning(self,msg):
-		self._format("warning",msg)
+		self._format("WARNING",msg)
 
 
+if __name__ == '__main__':
+	#tests
+	class test():
+		def __init__(self):
+			pass
+		def echo(self):
+			l = Logger()
+			l.info("hello from here")
+
+	a = test()
+	a.echo()
 
