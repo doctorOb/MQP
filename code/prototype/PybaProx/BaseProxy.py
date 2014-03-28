dce36f470facfc405c6a89fd885f3f017646d059
Dan Robertson <drob.drobertson@gmail.com>
2014-03-27 20:54:23 -0400

    more work

--- code/prototype/PybaProx/BaseProxy.py
+++ code/prototype/PybaProx/BaseProxy.py
@@ -161,6 +161,7 @@ class ProxyRequest(Request):
 		#A client factory for the ProxyCLient needs to be created. The factory is then passed to 
 		#the reactor which will call it when a TCP connection is established with the host
 		self.uri = self.host + self.rest
+		print self.uri
 		clientFacotry = class_(self.method, self.rest, self.clientproto, headers, s, self)
 		self.reactor.connectTCP(self.uri,self.port,clientFacotry)
 
