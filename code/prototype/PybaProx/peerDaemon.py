3c9c7d894c06fcc41c5f84c412070a0b8aa2fd16
Dan Robertson <drob.drobertson@gmail.com>
2014-03-29 14:55:11 -0400

    updated config file

--- code/prototype/PybaProx/peerDaemon.py
+++ code/prototype/PybaProx/peerDaemon.py
@@ -179,6 +179,7 @@ class Dispatcher(Resource):
 		ip = request.getClientIP()
 		try:
 			to_hash = "{}-{}".format(ip,headers['Target'][0])
+			print headers['Signature']
 			signature = (long(headers['Signature'][0]),) #has to be a tuple, with an empty second element (a crypto module restriction)
 
 			client_key = self.neighbors[ip].key
