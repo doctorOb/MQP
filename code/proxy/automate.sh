c1082a23c31632e9d74023ca187f3222e652f56e
Dan Robertson <drob.drobertson@gmail.com>
2014-04-08 11:50:37 -0400

    added uniform automate script with switch case for each role

--- code/proxy/automate.sh
+++ code/proxy/automate.sh
@@ -2,23 +2,63 @@
 #serve has files of size {1,5,10,25,50,75,100,200,300,400,500,600,700,800,900}M and {1,2,4,5,10}G housed at /sizes/${size}.test
 
 export SERVER_ROOT=http://10.18.234.114;
-export SIZE_URL=${SERVER_ROOT}/sizes/;
-export STUFF_URL=${SERVER_ROOT}/stuff/;
+export SIZE_URL=${SERVER_ROOT}/sizes;
+export STUFF_URL=${SERVER_ROOT}/stuff;
 
-SESSION_WAIT='2700'; #45 minutes
-
+LOG_FILE=nightly.log;
 
 function router() {
-	CHUNK_SIZE=$1;
-	THROTLE=$2; #not used by this script
-	PROXY_PATH=../prototype;
-
+	#kill the proxy if it is currently running, then start again with the new chunk size
+	CHUNK_SIZE=$2;
+	THROTLE=$3; #not used by this script
 
 	kill -9 `cat current_proc.pid` || true;
-	python $PROXY_PATH/proxy-main.py -c $1 -f $PROXY_PATH/module.cfg;
+	python proxy-main.py -c $CHUNK_SIZE;
 	echo $! > current_proc.pid;
 }
 
+function server() {
+	#clear the current tc queues attached to eth1, reapply them with the new limit, for all possible router IPs.
+	LIMIT=$2
+	sudo tc qdisc del dev eth1 root;
+	sudo tc qdisc add dev eth1 root handle 1: htb;
+	sudo tc class add dev eth1 parent 1:1 classid 1:11 htb rate $LIMIT ceil $LIMIT;
+	sudo tc class add dev eth1 parent 1:1 classid 1:12 htb rate $LIMIT ceil $LIMIT;
+
+	A_IP=10.18.175.187;
+	B_IP=10.18.211.123;
+	sudo tc filter add dev $DEV protocol ip parent 1:0 prio 1 u32 match ip dst ${A_IP} flowid 1:11;
+	sudo tc filter add dev $DEV protocol ip parent 1:0 prio 1 u32 match ip dst ${B_IP} flowid 1:12;
+}
+
+function client() {
+	SIZE_URL=$2
+	(time tcurl $SIZE_URL/$SIZE.test) &>> $LOG_FILE;
+}
+
+
+
+case "$1" in
+
+	router)
+		#do router stuff
+		router
+		;;
+	server)
+		#do server stuff
+		server
+		;;
+	client)
+		#do client stuff
+		client
+		;;
+	*)
+		#error
+		echo "Improper command piped from coordinator"
+		;;
+
+esac
+exit 0;
 
 
 
