177bbf4fcdef0dd2756de10aca54eb6bd3634cb5
Dan Robertson <drob.drobertson@gmail.com>
2014-04-10 00:50:08 -0400

    updated scripts

--- code/proxy/automate-client.py
+++ code/proxy/automate-client.py
@@ -7,7 +7,7 @@ from time import sleep
 import subprocess
 
 SERVER_IP="10.18.234.114"
-ROUTERS=['10.18.175.187','10.18.211.123','10.18.205.154']
+ROUTERS=['10.18.175.187','10.18.211.123','10.18.228.100']
 
 SERVER_ROOT="http://{}".format(SERVER_IP)
 
